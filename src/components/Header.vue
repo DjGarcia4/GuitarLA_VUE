1dc6365de00aaadd41bc8e6be00bbfeec873cefa
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2023-10-25 14:41:13 -0600

    Without Tooltips

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -45,7 +45,6 @@ const totalPay = computed(() => {
           d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
         />
       </svg>
-      <v-tooltip activator="parent" location="left">Cart</v-tooltip>
       <div
         v-if="cart.length > 0"
         class="w-2 h-2 bg-red-500 rounded-full animate-ping text-white"
@@ -103,9 +102,6 @@ const totalPay = computed(() => {
                     type="button"
                     @click="$emit('decrement-product', product.id)"
                   >
-                    <v-tooltip activator="parent" location="top"
-                      >Decrement Cant</v-tooltip
-                    >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -124,9 +120,6 @@ const totalPay = computed(() => {
                     type="button"
                     @click="$emit('increment-product', product.id)"
                   >
-                    <v-tooltip activator="parent" location="top"
-                      >Increment Cant</v-tooltip
-                    >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -147,9 +140,6 @@ const totalPay = computed(() => {
                   type="button"
                   @click="$emit('delete-product', product.id)"
                 >
-                  <v-tooltip activator="parent" location="top"
-                    >Delete Product</v-tooltip
-                  >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
@@ -179,7 +169,6 @@ const totalPay = computed(() => {
           class="bg-red-500 m-5 p-1 rounded-md text-white font-bold hover:bg-red-600 transition-colors flex justify-center"
           @click="$emit('empty-cart')"
         >
-          <v-tooltip activator="parent" location="bottom">Empty Cart</v-tooltip>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -215,9 +204,6 @@ const totalPay = computed(() => {
           class="bg-amber-500 m-5 p-3 rounded-md text-white font-bold hover:bg-amber-600 transition-colors"
           @click="$emit('add-cart', guitar)"
         >
-          <v-tooltip activator="parent" location="bottom"
-            >Add Product</v-tooltip
-          >
           Add to Cart
         </button>
       </div>
